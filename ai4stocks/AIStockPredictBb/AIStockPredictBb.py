f1329ca222cd317d85ab865d42f89563519c750e
Mark Chen <ktayler@students.solano.edu>
2023-09-06 19:55:49 +0800

    Add files via upload

---
+++ ai4stocks/AIStockPredictBb/AIStockPredictBb.py
@@ -0,0 +1,9 @@
+#import  AIStockPredictBbMain.*
+from AIStockPredictBbMain import AIStockPriceForecasting
+  
+AIStockPriceForecasting(
+      portfolio=["TSLA", "AAPL", "NVDA", "NFLX"], #stocks whose prices you want to forecast 
+      start_date = "2022-01-01", #starting date from there you want to take data to predict
+      weights = [0.2, 0.3, 0.3, 0.2], #assign 20% to TSLA, 30% to AAPL etc (default to equal weight )
+      prediction_days=30 #no of days for prediction
+)
